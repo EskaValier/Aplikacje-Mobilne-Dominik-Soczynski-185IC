b791b0ca248645f09e68b6fa7418efe86f4bb535
EskaValier <d.soczek1999@gmail.com>
2021-03-17 12:16:46 +0100

    Update App.js

--- Lab1/Kalkulator/App.js
+++ Lab1/Kalkulator/App.js
@@ -9,48 +9,77 @@ export default class App extends Component {
         input: '',
         inputValue: 0,
         inputValue2: 0,
-        result: 0
+        symbol: '',
+        result: 0,
+        errorMesage: '',
     }
     onButtonPressed = function() { this.setState({ text:this.state.inputValue })}
-    _handleTextChange = inputValue => { this.setState({ inputValue }); 
-    };
-    _handleTextChange2 = inputValue2 => { this.setState({ inputValue2 }); 
-    };
+    _handleTextChange = inputValue => { this.setState({ inputValue }); };
+    _handleTextChange2 = inputValue2 => { this.setState({ inputValue2 }); };
 
     add = function() {
-        let x = parseFloat(this.state.inputValue);
-        let y = parseFloat(this.state.inputValue2);
-        let result = x + y;
-        this.setState({ text: result})
+        this.setState({ symbol: "+"});
     }
 
     subtract = function() {
-        let x = parseFloat(this.state.inputValue);
-        let y = parseFloat(this.state.inputValue2);
-        let result = x - y;
-        this.setState({ text: result})
+        this.setState({ symbol: "-"});
     }
 
     divide = function(){
-        let x = parseFloat(this.state.inputValue);
-        let y = parseFloat(this.state.inputValue2);
-        let result = x / y;
-        this.setState({ text: result})
+        this.setState({ symbol: "/"});
     }
 
     multiplicate = function(){
-        let x = parseFloat(this.state.inputValue);
-        let y = parseFloat(this.state.inputValue2);
-        let result = x * y;
-        this.setState({ text: result})
+        this.setState({ symbol: "*"});
+    }
+
+    equal = function(){
+        switch(this.state.symbol){
+            case '+':
+                let x = parseFloat(this.state.inputValue);
+                let y = parseFloat(this.state.inputValue2);
+                let result = x + y;
+                this.setState({ text: result});
+                this.setState({ errorMesage: ""});
+                break;
+            case '-':
+                x = parseFloat(this.state.inputValue);
+                y = parseFloat(this.state.inputValue2);
+                result = x - y;
+                this.setState({ text: result});
+                this.setState({ errorMesage: ""});
+                break;
+            case '/':
+                x = parseFloat(this.state.inputValue);
+                y = parseFloat(this.state.inputValue2);
+                if(y==0){
+                    this.setState({ errorMesage: "Nie dziel przez 0"});
+                    this.setState({ text: "0"});
+                }
+                else{
+                    result = x / y;
+                    this.setState({ errorMesage: ""});
+                    this.setState({ text: result});
+                }
+                break;
+            case '*':
+                x = parseFloat(this.state.inputValue);
+                y = parseFloat(this.state.inputValue2);
+                result = x * y;
+                this.setState({ text: result});
+                this.setState({ errorMesage: ""});
+                break;
+            default:
+                break;
+        }
     }
 
     render() {
         return (
         <View style={styles.container}>
-            <Text style={{fontWeight: 'bold'}}> 
-            Pierwsza liczba
-            </Text>
+            <Text style={styles.headerStyle}> Kalkulator </Text>
+
+            <Text style={styles.textStyle}> Pierwsza liczba: </Text>
 
             <TextInput
             value={this.state.inputValue}
@@ -58,10 +87,25 @@ export default class App extends Component {
             onChangeText={this._handleTextChange}
             style={styles.textInputStyle}
             />
+            
+            <Text style={styles.textStyle}> Dzialanie:   {this.state.symbol} </Text>
 
-            <Text style={{fontWeight: 'bold'}}> 
-            Druga liczba
-            </Text>
+            <View style={styles.buttonContainer}>
+                <View style={styles.buttonStyle}>
+                    <Button title='+' onPress={this.add.bind(this)} style={styles.buttonStyle}/>
+                </View>
+                <View style={styles.buttonStyle}>
+                    <Button title='-' onPress={this.subtract.bind(this)} style={styles.buttonStyle}/>
+                </View>
+                <View style={styles.buttonStyle}>
+                    <Button title='/' onPress={this.divide.bind(this)} style={styles.buttonStyle}/>
+                </View>
+                <View style={styles.buttonStyle}>
+                    <Button title='*' onPress={this.multiplicate.bind(this)} style={styles.buttonStyle}/>
+                </View>
+            </View>
+
+            <Text style={styles.textStyle}> Druga liczba: </Text>
 
             <TextInput
             value={this.state.inputValue2}
@@ -70,24 +114,14 @@ export default class App extends Component {
             style={styles.textInputStyle}
             />
 
-            <Text style={styles.paragraph}>
-            {this.state.text}
-            </Text>
-        
-            <View style={styles.buttonContainer}>
-                <View style={styles.buttonStyle}>
-                <Button title='+' onPress={this.add.bind(this)} style={styles.buttonStyle}/>
-                </View>
-                <View style={styles.buttonStyle}>
-                <Button title='-' onPress={this.subtract.bind(this)} style={styles.buttonStyle}/>
-                </View>
-                <View style={styles.buttonStyle}>
-                <Button title='/' onPress={this.divide.bind(this)} style={styles.buttonStyle}/>
-                </View>
-                <View style={styles.buttonStyle}>
-                <Button title='*' onPress={this.multiplicate.bind(this)} style={styles.buttonStyle}/>
-                </View>
+            <View style={styles.buttonStyle}>
+                <Button title='=' onPress={this.equal.bind(this)} style={styles.buttonStyle}/>
             </View>
+            
+            <Text style={styles.textStyle, {color: 'red'}}> {this.state.errorMesage} </Text>
+
+            <Text style={styles.textStyle}> Wynik: </Text>
+            <Text style={styles.paragraph}> {this.state.text} </Text>
         </View>
         );
     }
@@ -99,7 +133,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#ecf0f1',
+        backgroundColor: 'darkgray',
     },
     paragraph: {
         fontSize: 18,
@@ -107,7 +141,8 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center',
         color: '#34495e',
-        borderWidth: 4, backgroundColor: 'yellow'
+        borderWidth: 2, 
+        backgroundColor: 'yellow',
     },
     buttonContainer: {
         flexDirection: 'row',
@@ -119,10 +154,21 @@ const styles = StyleSheet.create({
     },
     textInputStyle: {
         height: 40, 
-        borderColor: 'gray', 
+        borderColor: 'black', 
         borderWidth: 2,
         textAlign: 'center',
-        marginBottom:20,
+        marginBottom: 20,
+        backgroundColor: 'yellow',
+    },
+    textStyle: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    headerStyle: {
+        color: 'yellow',
+        fontWeight: 'bold',
+        fontSize: 30,
+        marginBottom: '30px',
     }
 
 });

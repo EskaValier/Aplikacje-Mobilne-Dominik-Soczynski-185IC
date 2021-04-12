3ca11a2f5c9ee4eb5135983cef36387c67fa111a
EskaValier <d.soczek1999@gmail.com>
2021-04-12 20:10:06 +0200

    Change_In_Styles

--- Lab2/lab2/components/Home.js
+++ Lab2/lab2/components/Home.js
@@ -3,11 +3,11 @@ import { Button, Text, View} from 'react-native';
 import styles from './styles'
 
 export default function Home({ navigation }) {
-  return (
-    <View style={styles.home.container}>
-        <Text style={styles.home.mainText}>Home</Text>
-        <Text style={styles.home.additionalText}>Menu utworzone z wykorzystaniem drawer-navigation. Wysuwa się je z lewej strony, lub za pomocą przyciski poniżej.</Text>
-        <Button styles={styles.home.button} onPress={() => navigation.openDrawer()} title="Otworz menu" />
-    </View>
-  );
+    return (
+        <View style={styles.home.container}>
+            <Text style={styles.home.title}>Home</Text>
+            <Text style={styles.home.text}>Menu utworzone z wykorzystaniem drawer-navigation. Wysuwa się je z lewej strony, lub za pomocą przyciski poniżej.</Text>
+            <Button styles={styles.home.button} onPress={() => navigation.openDrawer()} title="Otworz menu" />
+        </View>
+    );
 }
--- Lab2/lab2/components/styles.js
+++ Lab2/lab2/components/styles.js
@@ -5,21 +5,21 @@ const styles = {}
 styles.home = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#888',
+        backgroundColor: '#777',
         justifyContent: 'center',
         marginTop: 25,
     },
     button:{
-        marginHorizontal: 15,
+        backgroundColor: 'red',
     },
-    mainText: {
-        color:'#00f',
+    title: {
+        color:'#0af',
         fontSize: 25,
         fontWeight: 'bold',
         textAlign: 'center',
     },
-    additionalText: {
-        color:'#000',
+    text: {
+        color:'#0af',
         fontSize: 15,
         textAlign: 'left',
         marginHorizontal: 15,
@@ -30,61 +30,35 @@ styles.home = StyleSheet.create({
 
 styles.content = StyleSheet.create({
     container: {
-        backgroundColor: '#444',
-        paddingHorizontal:32,
-        paddingVertical:32,
-    },
-    example:{
         flex: 1,
-        backgroundColor: '#fa0',
-        justifyContent: 'center',
-        marginHorizontal: 16,
-        marginVertical: 32,
-        paddingHorizontal:16,
-        paddingVertical:16,
-        borderRadius:10,
+        marginTop: 25,
+        backgroundColor: '#555',
+        paddingHorizontal:30,
+        paddingVertical:30,
     },
-    text: {
-        color:'#fa0',
-        fontSize:20,
-    },
-    code:{
-        color:'#000',
-        fontSize:16,
-    }
-});
-
-styles.nav = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        backgroundColor: '#400',
-        paddingHorizontal:5,
-        paddingVertical:3,
-    },
-    text: {
-        color:'#000',
-        fontSize:20,
+    title: {
+        color:'#0af',
+        fontSize: 25,
+        fontWeight: 'bold',
         textAlign: 'center',
+        marginVertical: 15,
     },
-    button:{
-        flex:1,
-        backgroundColor: '#fa0',
-        alignItems: 'center',
-        justifyContent: 'center',
+    text: {
+        color:'#0af',
+        fontSize:20,
         marginHorizontal: 15,
-        marginVertical: 20,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        elevation: 10,
-        height:100,
-        borderRadius:10,
+        marginVertical: 30,
     },
-});
-  
+    textCode: {
+        backgroundColor: '#666',
+        color:'#0af',
+        fontSize:15,
+        padding: 10,
+        marginHorizontal: 15,
+    },
+    button: {
+
+    },
+});  
 
 export default styles

067361273fad2173c4b53ce9dbcf7b10d51d448a
EskaValier <d.soczek1999@gmail.com>
2021-04-19 21:55:22 +0200

    Make Sort using map()

--- Lab3/lab3/components/SortNumbers.js
+++ Lab3/lab3/components/SortNumbers.js
@@ -26,7 +26,7 @@ export default class SortNumbers extends Component{
         console.log(this.state.arr)
         const newarr = [];
         for (let i = 0; i < this.state.arr.length; i++) {
-        newarr.push(Math.floor(Math.random() * 1000) + 1);
+            newarr.push(Math.floor(Math.random() * 1000) + 1);
         }
         this.state.arr = newarr;
         this.state.arr.sort((a,b) => a > b ? 1:-1);
@@ -40,14 +40,13 @@ export default class SortNumbers extends Component{
                 <Text style={styles.content.title}>Sortowanie Liczb</Text>
                 <View style={styles.content.example}>
                     <View >
-                        <Text style={styles.content.code}>Wyświetlanie posortowanych liczb z przedziału od 1 do 1000.</Text>
+                        <Text style={styles.content.text}>Wyświetlanie wylosowanych i posortowanych liczb z przedziału od 1 do 1000.</Text>
                     </View>
-                    <View style={styles.content.buttons}>
-                        <Button  onPress={this.randomizeAndSort} title="Losuj i sortuj" />
+                    <View >
+                        <Button style={styles.content.button} onPress={this.randomizeAndSort} title="Losuj i sortuj" />
                     </View>
                 </View>
-
-                <View style={styles.content.example} >
+                <View style={styles.content.textCode} >
                     <NumbersList  liczby={this.state.arr} />
                 </View>
             </View>

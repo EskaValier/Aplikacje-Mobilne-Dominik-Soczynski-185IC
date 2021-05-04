ae9e4e7b542770069ffcb77aeca6490383067f78
EskaValier <d.soczek1999@gmail.com>
2021-05-04 10:46:30 +0200

    initial commit

---
+++ Lab5/lab5/components/styles.js
@@ -0,0 +1,72 @@
+import { StyleSheet } from 'react-native';
+
+const styles = {}
+
+styles.home = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#777',
+        justifyContent: 'center',
+        marginTop: 25,
+    },
+    button:{
+        backgroundColor: 'red',
+    },
+    title: {
+        color:'#0af',
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    text: {
+        color:'#0af',
+        fontSize: 15,
+        textAlign: 'left',
+        marginHorizontal: 15,
+        marginVertical: 15,
+    },
+    alignCenter: {
+        textAlign: 'center',
+    }
+});
+
+
+styles.content = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 25,
+        backgroundColor: '#555',
+        paddingHorizontal:30,
+        paddingVertical:30,
+    },
+    title: {
+        color:'#0af',
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginVertical: 15,
+    },
+    text: {
+        color:'#0af',
+        fontSize:20,
+        marginHorizontal: 15,
+        marginVertical: 30,
+    },
+    textCode: {
+        backgroundColor: '#666',
+        color:'#0af',
+        fontSize:15,
+        padding: 10,
+        marginHorizontal: 15,
+        marginBottom:30,
+    },
+    header: {
+        backgroundColor: '#777',
+        color:'#0af',
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    }
+});  
+
+export default styles

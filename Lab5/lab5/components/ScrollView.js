b62218b8c8f83f08d8c78b4ffcf238771488425e
EskaValier <d.soczek1999@gmail.com>
2021-05-04 10:46:45 +0200

    Create ScrollView.js

---
+++ Lab5/lab5/components/ScrollView.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import styles from './styles'
+
+export default class ScrollViewOne extends Component {
+    render(){ 
+        return (
+            <View style={styles.content.container}>
+                <Text style={styles.content.title}>Pierwszy ScrollView</Text>
+                <Text style={styles.content.text}>Standardowy ScorllView z ustawioną wysokością na 3/4 ekranu urządzenia na którym jest uruchamiana strona</Text>
+                <ScrollView style={styles.content.textCode}>
+                    <Text style={styles.content.title}>Lorem ipsum</Text>
+                    <Text style={styles.content.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                    <Text style={styles.content.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                    <Text style={styles.content.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                    <Text style={styles.content.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+                </ScrollView>
+            </View >
+        )
+    };
+}
